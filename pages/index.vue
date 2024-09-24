21d86b56890efb1c22c7ab9c845f077f2f72664a
gr91 <g.romagno91@gmail.com>
2024-09-24 18:57:15 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -7,14 +7,43 @@ useHead({
     { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
   ],
 });
+
+
 </script>
 
 <template >
-  <AppHeader />
+  <!--
+    <AppHeader />
+    <div>
+      <h1 class="text-4xl font-bold text-center text-blue-500">Nuxt Routing set up successfully!</h1>
+      <p>Current route: {{ route.path }}</p>
+      <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
+      <p>My placeholder paragraph in "index.vue"</p>
+    </div>
+  -->
+
+  <!-- Example from Decap nuxt/content installation process -->
   <div>
-    <h1 class="text-4xl font-bold text-center text-blue-500">Nuxt Routing set up successfully!</h1>
-    <p>Current route: {{ route.path }}</p>
-    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
-    <p>My placeholder paragraph in "index.vue"</p>
+    <li v-for="post of posts" :key="post.slug">
+      <NuxtLink :to="post.slug">{{ post.title }}</NuxtLink>
+    </li>
   </div>
+
+
 </template>
+
+
+<!---->
+Example from Decap nuxt/content installation process
+<script>
+export default {
+   async asyncData({ $content }) {
+    const posts = await $content("blog").fetch();
+
+    return {
+      posts,
+    };
+  },
+};
+
+</script>
