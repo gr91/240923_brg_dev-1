5a410470b1f9ca67aa267e444e9cb9fc889b6dc2
gr91 <g.romagno91@gmail.com>
2024-09-26 13:11:48 +0200

    Update index.vue

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,11 +1,27 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
-
 <template>
-    <main>
+    <main class="mx-auto max-w-7xl space-y-6">
         <AppHeader />
         <p>books/index.vue page</p>
+
+        <div v-for="post of posts" :key="post.slug">
+          <NuxtLink :to="post._path">
+            <div class="h-[52px] flex-col justify-start items-start inline-flex" >
+              <div class="self-stretch text-[#141414] text-sm font-normal font-['Neue Haas Unica W1G']">
+                <p>{{ post.date }}</p>
+              </div>
+              <div class="self-stretch text-[#141414] text-2xl font-normal font-['Neue Haas Unica W1G'] leading-[31.20px]">
+                <p>{{ post.title }}</p>
+              </div>
+            </div>         
+          </NuxtLink>
+        </div>
+
     </main>
 </template>
+
+<script setup lang="ts">
+const route = useRoute()
+const { data: posts } = await useAsyncData('posts', () => {
+    return queryContent('/books').find()
+  })
+</script>
