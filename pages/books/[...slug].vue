c344f47d47e3adcab3213e6bd7913455b5beecdf
gr91 <g.romagno91@gmail.com>
2024-09-26 13:29:48 +0200

    Create [...slug].vue

---
+++ pages/books/[...slug].vue
@@ -0,0 +1,27 @@
+<template>
+
+  
+    <main class="mx-auto max-w-7xl space-y-6">
+        <AppHeader />
+        <!-- <NuxtLink to="/blog">&lt; Back</NuxtLink> -->
+        <nav class="mb-8" aria-label="go back">
+            <router-back class="block" />
+          </nav>
+        <h1 class="text-4xl">
+          {{ post.title }}
+        </h1>
+        <p>{{ post.description }}</p>
+        <hr>
+        <ContentDoc />
+      </main>
+  </template>
+  
+  <script setup>
+  const route = useRoute()
+  
+  const actualPath = route.path.replace(/\/$/, '');
+  
+  const { data: post } = await useAsyncData(`hello`, () =>
+      queryContent(actualPath).findOne()
+  );
+  </script>
