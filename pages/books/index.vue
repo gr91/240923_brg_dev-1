e0c14ffb2f2cfdb56cbbc77970d8151b5340990c
gr91 <g.romagno91@gmail.com>
2024-09-30 19:52:48 +0200

    Update index.vue
    post -> book
    posts -> books

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-const route = useRoute()
-const { data: posts } = await useAsyncData('posts', () => {
+//const route = useRoute()
+const { data: books } = await useAsyncData('books', () => {
   return queryContent('/books').find()
 })
 </script>
@@ -11,14 +11,14 @@ const { data: posts } = await useAsyncData('posts', () => {
       <AppHeader />
       <p><strong>books</strong>/index.vue page</p>
 
-      <div v-for="post of posts" :key="post.slug">
+      <div v-for="book of books" :key="book.slug">
 
         <div class="h-[52px] flex-col justify-start items-start inline-flex" >
           <div class="self-stretch text-[#141414] text-sm font-normal font-['Neue Haas Unica W1G']">
-            <p>{{ post.date }}</p>
+            <p>{{ book.date }}</p>
           </div>
           <div class="self-stretch text-[#141414] text-2xl font-normal font-['Neue Haas Unica W1G'] leading-[31.20px]">
-            <p>{{ post.title }}</p>
+            <p>{{ book.title }}</p>
           </div>
          </div>    
               
