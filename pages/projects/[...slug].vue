cc541dd2d4945c50a48aed87978468735a79ad91
gr91 <g.romagno91@gmail.com>
2024-10-01 18:32:47 +0200

    Update [...slug].vue

--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -29,9 +29,12 @@ const { data: project } = await useAsyncData(`hello`, () =>
         </div>
     </div>
 
-    <div {{ project.video }}>    </div>
-
+    
     <!--
+        <div {{ project.video }}>
+            
+        </div>
+
         <div style="padding:75% 0 0 0;position:relative;">
             <iframe src="https://player.vimeo.com/video/1014856847?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="-2022 HE SHE ME  01_4">      
             </iframe>
