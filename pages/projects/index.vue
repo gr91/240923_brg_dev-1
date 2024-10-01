c3cedbd52f5be083e4b9f50b7fb1365354994809
gr91 <g.romagno91@gmail.com>
2024-10-01 12:14:01 +0200

    update new colleciton, create pages/projects/index

---
+++ pages/projects/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+//const route = useRoute()
+const { data: projects } = await useAsyncData('projects', () => {
+  return queryContent('/projects').find()
+})
+</script>
+
+<template>
+
+    <main class="mx-auto max-w-7xl space-y-6">
+      <AppHeader />
+      <p><strong>projects</strong>/index.vue page</p>
+
+      <div v-for="project of projects" :key="project.slug">
+          <div class="h-[52px] flex-col justify-start items-start inline-flex" >
+            <div class="self-stretch text-[#141414] text-sm font-normal font-['Neue Haas Unica W1G']">
+              <p>{{ project.beginyear }}</p>
+            </div>
+            <div class="self-stretch text-[#141414] text-2xl font-normal font-['Neue Haas Unica W1G'] leading-[31.20px]">
+              <p>{{ project.title }}</p>
+            </div>
+          </div>         
+      </div>
+    </main>
+
+</template>
+  
