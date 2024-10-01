ee56cbf81bf7ba3799e3213a26e0801c2975901e
gr91 <g.romagno91@gmail.com>
2024-10-01 14:03:40 +0200

    Update [...slug].vue

--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -14,14 +14,14 @@ const { data: project } = await useAsyncData(`hello`, () =>
     <AppHeader />
     
     <div class="h-[52px] flex-col justify-start items-start inline-flex" >
+        <div class="self-stretch text-[#141414] text-2xl font-normal font-['Neue Haas Unica W1G'] leading-[31.20px]">
+            <p>{{ project.title }}</p>
+        </div>
+        <div class="self-stretch text-[#141414] text-sm font-normal font-['Neue Haas Unica W1G']">
+          <p>{{ project.beginyear }}</p>
+        </div>
         <div class="self-stretch text-[#141414] text-sm font-normal font-['Neue Haas Unica W1G']">
           <p>{{ project.category }}</p>
-        </div>
-        <div class="self-stretch text-[#141414] text-sm font-normal font-['Neue Haas Unica W1G']">
-          <p>{{ project.title }}</p>
-        </div>
-        <div class="self-stretch text-[#141414] text-2xl font-normal font-['Neue Haas Unica W1G'] leading-[31.20px]">
-            <p>{{ project.beginyear }}</p>
         </div>
     </div>
     
