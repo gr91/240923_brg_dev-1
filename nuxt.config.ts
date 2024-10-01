d1bc81a10442becde7a26c3c2dce2352a5fad066
gr91 <g.romagno91@gmail.com>
2024-10-01 19:03:32 +0200

    Install nuxt i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: true,
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/content'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/content', '@nuxtjs/i18n'],
   content: {
     // Options
   }
