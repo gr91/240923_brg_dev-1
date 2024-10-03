36c1bc48e7664f18fdfbb0f6e5d3b7cd504ad903
gr91 <g.romagno91@gmail.com>
2024-10-03 18:48:19 +0200

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,26 +2,26 @@
     <span>
         <div style="border:solid;" class="sticky w-full h-28 mx-auto py-[16px] px-[16px] bg-[#e6e6e4] justify-between items-start inline-flex">
             <div class="justify-center items-center gap-2.5 flex">
-                <div class="text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/">Bruna Ginammi</NuxtLink></div>
+                <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/">Bruna Ginammi</NuxtLink></div>
             </div>
             <div class="justify-end items-center gap-8 flex">
                 <div class="justify-center items-center gap-2.5 flex">
-                    <div class="text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/artworks">Artworks</NuxtLink></div>
+                    <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/artworks">Artworks</NuxtLink></div>
                 </div>
                 <div class="justify-center items-center gap-2.5 flex">
-                    <div class="text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/books">Books</NuxtLink></div>
+                    <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/books">Books</NuxtLink></div>
                 </div>
                 <div class="justify-center items-center gap-2.5 flex">
-                    <div class="text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/projects">Projects</NuxtLink></div>
+                    <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/projects">Projects</NuxtLink></div>
                 </div>
                 <div class="justify-center items-center gap-2.5 flex">
-                    <div class="text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/custom-pages/bio">Bio</NuxtLink></div>
+                    <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/custom-pages/bio">Bio</NuxtLink></div>
                 </div>
                 <div class="justify-center items-center gap-2.5 flex">
-                    <div class="text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/custom-pages/contacts">Contacts</NuxtLink></div>
+                    <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/custom-pages/contacts">Contacts</NuxtLink></div>
                 </div>
                 <div class="justify-center items-center gap-2.5 flex">
-                    <div class="text-base font-normal font-['Neue Haas Unica W1G']">ITA</div>
+                    <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']">ITA</div>
                 </div>
             </div>
         </div>
