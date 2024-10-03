933468043ae2f98fef5f74c23a171a204e765f6d
gr91 <g.romagno91@gmail.com>
2024-10-03 18:43:20 +0200

    add postcss setup from tailwind doc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: true,
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   modules: [
     '@nuxt/content',
     '@nuxtjs/i18n',
