9d0df56cbead6fcfce38c6b900ebc49b1226091c
gr91 <g.romagno91@gmail.com>
2024-10-03 19:16:01 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: true,
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
