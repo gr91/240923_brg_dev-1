0e1cb034913b622e0e848de961798288dac8b98f
gr91 <g.romagno91@gmail.com>
2024-10-08 14:58:13 +0200

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <span>
-        <div class="sticky w-full h-28 mx-auto py-[16px] bg-BRG-white justify-between items-start inline-flex">
+        <div style="border:solid black" class="sticky w-full h-28 mx-auto py-[16px] bg-BRG-white justify-between items-start inline-flex">
             <div class="justify-center items-center gap-2.5 flex">
                 <div class="text-black text-base font-normal font-['Neue Haas Unica W1G']"><NuxtLink to="/">Bruna Ginammi</NuxtLink></div>
             </div>
