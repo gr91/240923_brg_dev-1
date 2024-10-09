ada44119f49512d80bd081119cb4225057c0b22c
gr91 <g.romagno91@gmail.com>
2024-10-09 17:11:25 +0200

    App structure 2

--- app.vue
+++ app.vue
@@ -1,16 +1,19 @@
 <template>
 
-  <!---------MAIN-------->
-  <main style="border:solid, red" class="mx-auto max-w-7xl h-svh px-4 flex flex-col">
-
-
-      <div style="border:solid black" class=" w-full">
-        <AppHeader />
+  <!--MAIN-->
+  <!--Set max width for the whole app-->
+  <main style="border:solid, red" class="mx-auto max-w-7xl">
+    
+      <!--APP HEADER CONTAINER-->
+      <!--Fix AppHeader at the top of the page and make it respect position and width -->
+      <div style="border:solid black" class="fixed mx-auto w-full max-w-7xl">
+        <AppHeader />  
+        <!--
+        -->
       </div>
 
-      <!---------AVAILABLE SPACE-------->
+      <!--PAGES-->
       <NuxtPage />
-     
     
   </main>
 </template>
--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -6,78 +6,91 @@ const actualPath = route.path.replace(/\/$/, '');
 const { data: project } = await useAsyncData(`hello`, () =>
     queryContent(actualPath).findOne()
 );
-
-
-
 </script>
 
 
 
-
+<!--Nuxt page-->
 <template>
-        <div style="border:solid black" class="grow overflow-scroll">
 
-            <div class="flex flex-col">
+    <!--FIT TO SCREEN-->        
+    <div style="border:solid, black" class="w-full h-svh mx-auto mb-1 p-4 flex flex-col">
 
-                <div class="flex flex-row justify-between">
-                    <div style="border:solid green" class=" flex-col justify-start items-start inline-flex" >
-                        <div class="self-stretch brg-body-text">
-                            <p>{{ project.beginyear }}—{{ project.endyear }}</p>
-                        </div>
-                        <div class="self-stretch brg-heading-text">
-                            <p style="font-size: 32px;">{{ project.title }}</p>
-                        </div>
-                    </div> 
-                    <div style="border: solid greenyellow" class=" flex justify-start items-start gap-4">
-                        <div class="self-stretch brg-body-text">Images</div>
-                        <div class="self-stretch brg-body-text">Texts</div>
+        <!--Empty section alligned to AppHeader / fixed height-->
+        <div style="border:solid, blue" class="w-full h-24 mb-1 flex-none "></div>
+        
+        <!--Project Heading section + navigation buttons / fixed height-->
+        <div style="border:solid, blue" class="w-full h-24 mb-1 flex-none ">
+            <div style="border:solid, aqua" class="bg-BRG-white flex flex-row justify-between ">
+                
+                <div style="border:solid green" class=" flex-col justify-start items-start inline-flex" >
+                    <div class="self-stretch brg-body-text">
+                        <p>{{ project.beginyear }}—{{ project.endyear }}</p>
+                    </div>
+                    <div class="self-stretch brg-heading-text">
+                        <p style="font-size: 24px;">{{ project.title }}</p>
+                    </div>
+                </div> 
+                
+                <div style="border: solid greenyellow" class=" flex justify-start items-start gap-4">
+                    <div class="self-stretch brg-body-text">Images</div>
+                    <div class="self-stretch brg-body-text">Texts</div>
+                </div>
+                
+            </div>
+        </div>
+        
+        <!--Images Carousel section / fill height-->
+        <div style="border: solid blue" class="w-full flex-auto">
+            <p>Images carousel</p>
+        </div>
+
+    </div>
+
+
+
+    <!--SCROLL SECTION with texts-->        
+    <div style="border: solid black" class="h-svh ">
+        <p>{{ project.bodyita }}</p>
+    </div>
+            
+            
+            
+        
+        
+            <!--
+                <div style="border:solid, blue" class="w-full mx-auto p-1 ">
+                    <div  v-for="(img, index) in project.images" key="index" >
+                        <img class="w-80 mx-auto" :src="img.image" />
+                        <p>{{ img.caption }}</p>
+                        <hr>
                     </div>
                 </div>
+            -->
 
-                <!--
-                    <div style="border:solid, blue" class="w-full mx-auto p-1 ">
+
+
+
+            <!--
+                <div style="border: solid aquamarine" class="mt-28 h-screen">
+                </div>
+            -->
+                
+                
+                
+                
+                
+            <!--
+                <div style="border: solid blue" id="controls-carousel" class="w-full grow">
+                    <div style="border:solid cyan" class="relative overflow-hidden ">
                         <div  v-for="(img, index) in project.images" key="index" >
-                            <img class="w-80 mx-auto" :src="img.image" />
+                            <img :src="img.image" class="absolute block h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                             <p>{{ img.caption }}</p>
                             <hr>
                         </div>
                     </div>
-                -->
-
-                <div style="border:solid, blue" class="w-full mx-auto p-1 h-svh">
-                    <p>My paragraph</p>
-                </div>
-
-                <div style="border: solid black">
-                    <p>{{ project.bodyita }}</p>
-                </div>
-
-            </div>
-            
-        </div>
-            
-            <!--
-                <div style="border: solid aquamarine" class="mt-28 h-screen">
-                </div>
-                -->
-                
-                
-                
-                
-                
-                <!--
-                    <div style="border: solid blue" id="controls-carousel" class="w-full grow">
-                        <div style="border:solid cyan" class="relative overflow-hidden ">
-                            <div  v-for="(img, index) in project.images" key="index" >
-                                <img :src="img.image" class="absolute block h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                                <p>{{ img.caption }}</p>
-                                <hr>
-                            </div>
-                        </div>
-                    </div>
-
-                    
-    -->    
+                </div>   
+            -->    
 
 
 
