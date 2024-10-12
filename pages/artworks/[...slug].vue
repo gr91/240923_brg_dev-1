bcfca6cf330570c4131c6d47f67558a58acf5088
gr91 <g.romagno91@gmail.com>
2024-10-12 11:44:11 +0200

    Revert "Fix content update ---3"
    This reverts commit 4126e504acf949214c834cdf814777820f3b1e05.

--- pages/artworks/[...slug].vue
+++ pages/artworks/[...slug].vue
@@ -3,7 +3,7 @@ const route = useRoute()
       
 const actualPath = route.path.replace(/\/$/, '');
       
-const { data: artwork } = useAsyncData(`helloSlug`, () =>
+const { data: artwork } = await useAsyncData(`helloSlug`, () =>
     queryContent(actualPath).findOne()
 );
 
