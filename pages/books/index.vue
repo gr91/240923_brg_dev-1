e7a4c4642da910d4f59e7bae125afca20d57bb70
gr91 <g.romagno91@gmail.com>
2024-10-14 19:09:39 +0200

    update book/index for i18n

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,11 +1,37 @@
 <script setup lang="ts">
 //const route = useRoute()
 const { data: books } = await useAsyncData('books', () => {
-  return queryContent('/books').find()
+  return queryContent('/books')
+  .sort({year: -1})
+  .find()
 })
 </script>
 
 <template>
+
+
+  <!---- SET NEW PATH FOR DATA QUERY ---->
+  <div class="mt-24" style="border:solid">
+    <p>{{ books }}</p>
+
+  </div>
+
+  <div v-for="book in books" :key="book.slug" class="w-full my-4" style="border:solid grey">
+    <div>
+      <p>{{ book.year }}</p>
+      <h3>{{ book.title }}</h3>
+
+      <br>
+      <div style="color: red;">
+        <MDC :value="book.truedes" />
+      </div>
+
+      <br>
+      <div style="color: blue">
+        <MDC :value="book.dupldes" />
+      </div>
+    </div>
+  </div>
 
   <!--
     <p><strong>books</strong>/index.vue page</p>
