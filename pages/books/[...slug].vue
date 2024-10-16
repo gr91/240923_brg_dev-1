e8ad80020f22582e30ed796d896f0ea0140941ea
gr91 <g.romagno91@gmail.com>
2024-10-16 19:12:18 +0200

    Revert "clean up index + slug from artworks and books"
    This reverts commit 2908b8119395ea1201d900ea3c3153a0e35e96f9.

--- pages/books/[...slug].vue
+++ pages/books/[...slug].vue
@@ -1,11 +1,61 @@
 <script setup>
 
+//------i18n configuration --- from nuxtjs/i18n docs
+const localePath = useLocalePath()
+const { locale, setLocale } = useI18n()
+
+
+const route = useRoute()      
+const actualPath = route.path.replace(/\/$/, '');
+//edit path to include en/it folder 
+const myPath = "/books" + actualPath.replace("/books", "");
+
+const myKey = myPath.replace("/books/", "").replace("/","-");
+
+const { data: book } = await useAsyncData(myKey, () =>
+    queryContent(myPath).findOne()
+);
 
 </script>
 
 
 
 <template>
+
+    <div class="h-40">
+    </div>
+
+    <div style="border:solid" class="mb-4">
+        <p><strong>book array</strong></p>
+        <p>{{ book }}</p> <br>
+        <p>{{ book.title }}</p> <br>
+        <p>{{ book.year }}</p> <br>
+        <MDC :value="book.truedes"/> <br>
+        <MDC :value="book.dupldes"/> <br>
+    </div>
+
+
+    <div style="border:solid" class="mb-4">
+        
+        <p><strong>Locale</strong></p>
+        <p>{{ locale }}</p> <br>
+
+        <p><strong>LocalePath</strong></p>
+        <p>{{ localePath }}</p> <br>
+        
+        <p><strong>Route</strong></p>
+        <p>{{ route }}</p> <br>
+        
+        <p><strong>actualPath</strong></p>
+        <p>{{ actualPath }}</p> <br>
+        
+        <p><strong>myPath</strong></p>
+        <p style="color: red">{{ myPath }}</p> <br>
+        
+        <p>{{ myPath }}</p>
+        <p>{{ myKey }}</p>
+
+    </div>
 
 
 </template>
--- pages/books/index.vue
+++ pages/books/index.vue
@@ -1,7 +1,75 @@
 <script setup lang="ts">
+//const route = useRoute()
+const { data: books } = await useAsyncData('books', () => {
+  return queryContent('/books')
+  .sort({year: -1})
+  .find()
+});
+
+//------i18n configuration --- from nuxtjs/i18n docs
+const localePath = useLocalePath()
+const { locale, setLocale } = useI18n()
+
+// const switchLocalePath = useSwitchLocalePath()
+// const localePath = useLocalePath()
+
+
+
 </script>
 
 <template>
 
+
+  <!---- SET NEW PATH FOR DATA QUERY ---->
+  <div class="mt-24" style="border:solid">
+    <p>{{ books }}</p>
+
+  </div>
+
+  <div v-for="book in books" :key="book.slug" class="w-full my-4" style="border:solid grey">
+  
+    <div>
+      <p>{{ book.year }}</p>
+      <h3>{{ book.title }}</h3>
+
+      <br>
+      <div style="color: red;">
+        <MDC :value="book.truedes" />
+      </div>
+
+      <br>
+      <div style="color: blue">
+        <MDC :value="book.dupldes" />
+      </div>
+    </div>
+  </div>
+
+
+  <div>
+    <p>Current language is set to <strong>{{ locale }}</strong></p>
+  </div>
+
+
+
+
+
+
+
+  <!--
+    <p><strong>books</strong>/index.vue page</p>
+  
+    <div v-for="book of books" :key="book.slug">
+  
+      <div class="h-[52px] flex-col justify-start items-start inline-flex" >
+        <div class="self-stretch text-[#141414] text-sm font-normal font-['Neue Haas Unica W1G']">
+          <p>{{ book.date }}</p>
+        </div>
+        <div class="self-stretch text-[#141414] text-2xl font-normal font-['Neue Haas Unica W1G'] leading-[31.20px]">
+          <p>{{ book.title }}</p>
+        </div>
+       </div>    
+            
+    </div>
+  -->
 
 </template>
