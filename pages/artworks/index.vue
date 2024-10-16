a6092c392efaeaa3c4d648447fa37d51c51c917a
gr91 <g.romagno91@gmail.com>
2024-10-16 19:37:24 +0200

    Revert "Clean up artworks/ index + slug"
    This reverts commit 60926571f859c75bf5281c95c6c991c2e8ded280.

--- pages/artworks/index.vue
+++ pages/artworks/index.vue
@@ -1,8 +1,80 @@
 <script setup lang="ts">
+//const route = useRoute()
+const { data: artworks } = await useAsyncData('hello', () => {
+  return queryContent('/artworks')
+  .sort({beginyear: 1, endyear: 1})
+  .find()
+});
+
+//------i18n configuration --- from nuxtjs/i18n docs
+const { locale, setLocale } = useI18n();
+
+
+
+//const { data: myNewCont, data, status, error, refresh, clear } = await useFetch('/artwkors', {
+//    pick: ['title']
+
+//})
+
+
 
 </script>
 
 <template>
     
+    <!--ARTWORK HEADER -->
+        
+    <!--Container for Header ans AppHeader alignment / fixed positionheight background fill
+    <div style="border:solid, blue, 3px; background-color: antiquewhite;" class="fixed z-10 mx-auto w-full max-w-7xl pr-8"> 
+    </div>
+    -->
+
+ 
+        
+    <!--ARTWORK Heading section + navigation buttons-->
+    <div class="h-32 md:h-24"></div>
+
+    <div style="border:solid" class="sticky top-12 md:top-24 bg-BRG-white  h-12 flex flex-row gap-4 items-center md:justify-end"> <!--style="border: solid greenyellow"-->
+        <div style="border-bottom: solid;" class="h-5 brg-body-text">{{ $t('photos') }}</div>
+        <div style="border-bottom: solid;" class="h-5 brg-body-text">{{ $t('videos') }}</div>
+        <div style="border-bottom: solid;" class="h-5 brg-body-text">{{ $t('other') }}</div>
+    </div>
+
+        
+    <!--Empty section alligned to AppHeader / fixed height-->
+    <!--
+        <div style="border:solid lightgray" class="w-full h-48 md:h-24 flex-none "></div>
+    -->
+    
+    <p>The current language is {{ locale }}</p>
+
+    <div v-for="artwork of artworks" :key="artwork.slug" class="my-4">
+        <NuxtLink :to="artwork._path">
+            <div  class=" flex-col justify-start items-start inline-flex" > <!--style="border:solid green"-->
+                <div class="self-stretch brg-body-text">
+                    <p v-if="artwork.beginyear != artwork.endyear"> {{ artwork.beginyear }}—{{ artwork.endyear }} </p>
+                    <p v-else>{{ artwork.beginyear }}</p>
+                </div>
+                <div class="self-stretch brg-heading-text">
+                    <h1 style="font-size: 24px; line-height: 130%;">{{ artwork.title }}</h1>
+                    <h1 style="font-size: 24px; line-height: 130%;">{{ artwork.subtitle }}</h1>
+                </div>
+            </div>
+                    
+        </NuxtLink>
+    </div>
+    
+
+    <!--
+        <br><br>
+        <p><strong>Artworks</strong> contains:</p><br><p>{{ artworks }}</p>
+        <br><br>
+        <p><strong>myNewCont</strong> contains:</p><br><p>{{ myNewCont }}</p>
+    -->
+
+
+
+
+    <div class="w-full h-svh"></div>
 
 </template>
