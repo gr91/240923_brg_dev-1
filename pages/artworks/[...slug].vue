1a01a91b8ec47a93494936ec19b8b664cec229df
gr91 <g.romagno91@gmail.com>
2024-10-17 18:24:17 +0200

    Rebuilt artworks/ index + slug

--- pages/artworks/[...slug].vue
+++ pages/artworks/[...slug].vue
@@ -1,6 +1,32 @@
 <script setup>
 
+//------i18n configuration --- from nuxtjs/i18n docs
+const localePath = useLocalePath()
+const { locale, setLocale } = useI18n()
 
+//Redefine [...slug] path to match content dir architecture
+//  content                             pages
+//  ---artworks                            ---artworks
+//  ------it | en                       ------ADD HERE /it | /en TO THE PATH  
+//  ------------[slug].md               ---------[...slug].vue
+defineI18nRoute({
+    paths: {
+      it: '/artworks/it/[...slug]',
+      en: '/artworks/en/[...slug]'
+    }
+  })
+
+
+
+const route = useRoute()      
+//edit actualPath to match the correct content path
+const actualPath = route.path.replace(/\/$/, '').substring(3);
+//pruce a unique key  
+const myKey = actualPath.replace("/artworks/", "").replace("/","-");
+
+const { data: artwork } = await useAsyncData(myKey, () =>
+    queryContent(actualPath).findOne()
+);
 
 </script>
 
@@ -9,6 +35,40 @@
 <!--Nuxt page-->
 <template>
 
+
+    <div class="mt-80">
+        <p><strong>Actual Path</strong></p>
+        <p>{{ actualPath }}</p>
+        
+        <br>
+        <p><strong>myKey</strong></p>
+        <p>{{ myKey }}</p>
+        
+        <br>
+        <p><strong>Artwork</strong></p>
+        <p>{{ artwork }}</p>
+
+        <br>
+        <p><strong>Artwork title</strong></p>
+        <p>{{ artwork.title }}</p>
+
+        <br>
+        <p><strong>Artwork begin year</strong></p>
+        <p>{{ artwork.beginyear }}</p>
+
+        <br>
+        <p><strong>Artwork end year</strong></p>
+        <p>{{ artwork.endyear }}</p>
+
+        <br>
+        <p><strong>Artwork body text</strong></p>
+        <p>{{ artwork.bodytext }}</p>
+
+        <br>
+        <p><strong>Artwork duplicate description</strong></p>
+        <p>{{ artwork.images }}</p>
+
+    </div>
 
 
 
--- pages/artworks/index.vue
+++ pages/artworks/index.vue
@@ -1,8 +1,81 @@
 <script setup lang="ts">
 
+//------i18n configuration --- from nuxtjs/i18n docs
+const localePath = useLocalePath()
+const { locale, setLocale } = useI18n()
+
+const route = useRoute()      
+const actualPath = route.path.replace(/\/$/, '');
+//edit path to include en/it folder 
+const myPath = "/artworks" + actualPath.replace("/artworks", "");
+
+//pruce a unique key  
+const myKey = myPath.replace('/artworks/', 'Helloartworks-');
+
+//const route = useRoute()
+const { data: artworks } = await useAsyncData(myKey, () => {
+  return queryContent(myPath)
+  .sort({year: -1})
+  .find()
+});
+
+
+// const switchLocalePath = useSwitchLocalePath()
+// const localePath = useLocalePath()
+
+
+
 </script>
+
+
+
+
 
 <template>
     
+      <!---- SET NEW PATH FOR DATA QUERY ---->
+    <div class="mt-24" style="border:solid">
+
+        <p><strong>actualPath</strong></p>
+        <p>{{ actualPath }}</p>
+        <br>
+
+        <p><strong>actualPath</strong></p>
+        <p>{{ myPath }}</p>
+        <br>
+        
+        <p><strong>actualPath</strong></p>
+        <p>{{ myKey }}</p>
+        <br>
+        
+    </div>
+
+
+
+    <div v-for="artwork in artworks" :key="artwork.slug" class="w-full my-4" style="border:solid grey">
+  
+        <div>
+            <p>{{ artwork.year }}</p>
+            <h3>{{ artwork.title }}</h3>
+
+            <br>
+            <div style="color: red;">
+            <MDC :value="artwork.truedes" />
+            </div>
+            <div style="color: blue">
+            <MDC :value="artwork.dupldes" />
+            </div>
+            
+            <br>
+            <div>
+            <p>{{ artwork._path }}</p>
+            </div>
+
+            <br>
+            <NuxtLink :to="localePath(String(artwork._path))"> See the artwork </NuxtLink>
+        </div>
+
+    </div>
+
 
 </template>
