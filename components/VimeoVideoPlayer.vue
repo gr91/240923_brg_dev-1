8db628b3beb284735554110a1a89b1674a83d57b
gr91 <g.romagno91@gmail.com>
2024-11-07 12:30:13 +0100

    create VimeoVideoPlayer component

---
+++ components/VimeoVideoPlayer.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+
+
+const VimeoPlayerProps = defineProps(['VideoId'])
+const SourceVideoId = `https://player.vimeo.com/video/${VimeoPlayerProps.VideoId}`;
+
+
+
+
+
+
+
+// Compute the Vimeo embed URL based on the provided vimeoId or vimeoUrl
+//const tempId = '1026152179';
+//const vimeoEmbedId = `https://player.vimeo.com/video/${tempId}`;
+
+
+
+</script>
+
+
+
+<template>
+    <p><strong>VideoId</strong>: {{ VideoId }}</p>
+    <p><strong>SourceVideoId</strong>: {{ SourceVideoId }}</p><br>
+
+
+    <div style="padding:56.25% 0 0 0;position:relative;">
+
+        <iframe
+            :src="SourceVideoId"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+            style="position:absolute;top:0;left:0;width:100%;height:100%;"
+        >
+        </iframe>
+
+    </div>
+
+</template>
+
+
