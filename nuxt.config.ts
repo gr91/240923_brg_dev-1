b677a6c1fbea64a12925ede9e025c11231622fc4
gr91 <g.romagno91@gmail.com>
2024-11-14 16:09:50 +0100

    Add google font links + update css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,11 @@ export default defineNuxtConfig({
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
+      link: [
+        {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+        {rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap'},
+      ],
     }
   },
 
