5e5e81de1a30d4c025a751d33e15029e7ae17249
gr91 <g.romagno91@gmail.com>
2024-11-22 14:06:18 +0100

    update modal

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -18,13 +18,20 @@ const { data: books } = await useAsyncData(myKey, () => {
 
 
 //nuxt ui modal config
-const isOpen = ref(false);
+const modalMediaImageIsOpen = ref(false);
+const modalImage = ref({value: ''});
+const modalCaption = ref({value: ''});
 
-const modalImage = ref({value: 'asd'});
 function updateThisMediaImage (
     modalImage: {value: string},
     currentImage: string) {
         modalImage.value = currentImage;
+};
+
+function updateThisMediaCaption (
+    modalCaption: {value: string},
+    currentCaption: string) {
+        modalCaption.value = currentCaption;
 };
 
 
@@ -84,47 +91,92 @@ function updateThisMediaImage (
 
 
             <!--MEDIA SESSION-->
-            <div> <!--style="border: solid red;"-->
+            <div>
+
 
 
 
                 <!--IMAGES-->
                 <div v-if="book.mediaimages" class="mb-8"> 
 
-                    <div class="max-w-[320px] md:max-w-sm flex flex-wrap items-start gap-2 mb-2"> <!--style="border: solid green;"-->
+                    <div class="max-w-[320px] md:max-w-sm flex flex-wrap items-start gap-2 mb-2">
+
                         <button v-for="image in book.mediaimages"
-                            @click="isOpen = true; updateThisMediaImage(modalImage, image.image)"
-                            class=""
+                            @click="
+                                modalMediaImageIsOpen = true;
+                                updateThisMediaImage(modalImage, image.image)
+                                updateThisMediaCaption(modalCaption, book.mediaimagescaption)"
                         >
+
                             <img :src="image.image" class="h-20 object-container">
+
                         </button>
+
                     </div>
 
                     <MDC :value="book.mediaimagescaption" class="brg-txt-caption"/>                    
                     
                 </div>
-                
+
+                <!--IMAGE MODAL-->
                 <UModal
-                v-model="isOpen"
-                fullscreen
-                :ui="{
-                    overlay: {background: 'bg-BRG-white opacity-50'}
-                    }"
+                    v-model="modalMediaImageIsOpen"
+                    :ui="{ overlay: {background: 'bg-BRG-white opacity-95'} }"
+                    fullscreen
                 >
-                    <div class="w-full h-full max-w-5xl m-auto flex px-8 md:px-0 py-4 md:py-16" > <!--style="border: solid red"-->
-                        <div class="m-auto w-full h-full flex" > <!--style="border: solid blue;"-->
+                    <div class="h-full w-full  mx-auto px-4 pb-12 pt-2 flex flex-col gap-2" >
+
+                        <div class="h-8 flex-none flex justify-between items-center " >
+
+                            <MDC :value="modalCaption.value" class="brg-txt-body"/>
+
+                            <button
+                                @click="modalMediaImageIsOpen = false"
+                                class="brg-cta brg-txt-button h-4 "
+                            >
+                                {{ $t('close') }}
+                            </button>
+
+                        </div>
+
+                        <div class="flex-1 overflow-hidden flex w-fit mx-auto" >
+                            <img :src="modalImage.value"
+                                class="object-contain h-full w-full"
+                            >
+                        </div>
+
+                    </div>            
+                    
+                </UModal>
+
+
+                <!--
+                <UModal
+                v-model="modalMediaImageIsOpen"
+                fullscreen
+                :ui="{ overlay: {background: 'bg-BRG-white opacity-50'} }"
+                >
+                    <div
+                        class="w-full h-full max-w-5xl m-auto flex p-4"
+                        style="border: solid red"
+                    > 
+                        <div
+                            class="m-auto w-full h-full flex"
+                            style="border: solid blue;"
+                        > 
                             
                             <img
-                            :src="modalImage.value"
-                            class="object-contain h-full w-full m-auto"
+                                :src="modalImage.value"
+                                class="object-contain h-full w-full m-auto"
                             >
                             
                         </div>
                     </div>
                 </UModal>
-            
-            
-            
+                -->
+
+
+
             
                 <!--VIDEO-->
                 <div v-if="book.mediavideo" class="mb-8" > <!---->
@@ -137,11 +189,13 @@ function updateThisMediaImage (
                 
                 </div>
         
+
+
         
             </div>
     
 
-            
+
 
         </div>
     
