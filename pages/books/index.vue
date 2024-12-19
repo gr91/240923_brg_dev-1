ef5290558ab5a500fb20a24c485d7d1a0a773d80
gr91 <g.romagno91@gmail.com>
2024-12-19 11:00:58 +0100

    books/index modal restore

--- pages/books/index.vue
+++ pages/books/index.vue
@@ -16,6 +16,23 @@ const { data: books } = await useAsyncData(myKey, () => {
     .find()
 });    
 
+//---------------- MODAL SETTING ----------------------
+const modalArtImageIsOpen = ref(false);
+
+// Define the reactive object
+const modalImage = ref({ value: '' });
+const modalCaption = ref({ value: '' });
+
+// Update function without TypeScript type annotations
+function updateThisMediaImage(modalImage: { value: any; }, currentImage: any) {
+  modalImage.value = currentImage;
+}
+
+// Update function without TypeScript type annotations
+function updateThisMediaCaption(modalCaption: { value: any; }, currentCaption: any) {
+    modalCaption.value = currentCaption;
+}
+
 
 
 
@@ -31,7 +48,14 @@ const { data: books } = await useAsyncData(myKey, () => {
         <div v-for="book in books" :key="book.slug" class="w-full max-w-[320px] md:max-w-sm pb-12 mb-12" > <!--style="border-bottom: solid lightgray;"-->
             
             <!--COVER-->
-            <div class="drop-shadow-xl w-full max-w-[320px] md:max-w-sm">
+            <button
+                @click="
+                    updateThisMediaImage(modalImage, book.cover);
+                    updateThisMediaCaption(modalCaption, book.title)
+                    modalArtImageIsOpen = true;
+                "
+                class="drop-shadow-xl w-full max-w-[320px] md:max-w-sm"
+                >
                 <div
                     >
                     
@@ -39,19 +63,10 @@ const { data: books } = await useAsyncData(myKey, () => {
                         :src="book.cover"
                         class="w-full mb-4"
                     >
-                    <!--
-                        <NuxtImg
-                        :src="book.cover"
-                        preload
-                        sizes="400px md:1200px"
-                        format="webp"
-                        class="w-full mb-4"
-                        />
-                    -->
+                </div>
+            </button>
 
-            </div>
-            </div>
-            
+
             <!--HEADING-->
             <div class="mb-4">
                 <p class="brg-txt-body">{{ book.year }}</p>
@@ -59,11 +74,13 @@ const { data: books } = await useAsyncData(myKey, () => {
                 <h1 class="brg-txt-heading">{{ book.subtitle }}</h1>
             </div>
             
+
             <!--TEXT-->
             <div class="mb-4 brg-txt-body">
                 <MDC :value="book.description" />
             </div>
             
+
             <!--BUTTON-->
             <div class="mt-6 mb-8">
                 <div class="w-fit brg-txt-button">
@@ -85,31 +102,25 @@ const { data: books } = await useAsyncData(myKey, () => {
             <!--MEDIA SESSION-->
             <div>
 
-
                 <!--IMAGES-->
                 <div v-if="book.mediaimages" class="mb-8"> 
-
                     <div class="max-w-[320px] md:max-w-sm flex flex-wrap items-start gap-2 mb-2">
+                        <div v-for="image in book.mediaimages">
 
-                        <div v-for="image in book.mediaimages"
-                            >
+                            <button
+                                @click="
+                                    updateThisMediaImage(modalImage, image.image);
+                                    updateThisMediaCaption(modalCaption, book.mediaimagescaption)
+                                    modalArtImageIsOpen = true;
+                                "
+                                >
+                                <img
+                                    :src="image.image"
+                                    class="h-20 object-container"
+                                >
+                            </button>
 
-                            <img
-                                :src="image.image"
-                                class="h-20 object-container"
-                            >
-                            <!--
-                                <NuxtImg
-                                preload
-                                :src="image.image"
-                                format="webp"
-                                sizes="120px md:400px"
-                                class="h-20 object-container"
-                                />
-                            -->
-
-                    </div>
-
+                        </div>
                     </div>
 
                     <MDC :value="book.mediaimagescaption" class="brg-txt-caption"/>                    
@@ -140,6 +151,44 @@ const { data: books } = await useAsyncData(myKey, () => {
         </div>
     
     </div>
+
+
+    <!--IMAGE MODAL-->
+    <UModal
+        v-model="modalArtImageIsOpen"
+        :ui="{ overlay: {background: 'bg-BRG-white opacity-95'} }"
+        fullscreen
+        >
+
+        <div class="h-full w-full  mx-auto px-4 pb-12 pt-2 flex flex-col gap-2">
+
+            <div class="h-8 flex-none flex justify-between items-center ">
+
+                <MDC :value="modalCaption.value" 
+                    class="brg-txt-body"
+                />
+
+                <button
+                    @click="modalArtImageIsOpen = false"
+                    class="brg-cta brg-txt-button h-4 "
+                    >
+                    {{ $t('close') }}
+                </button>
+
+            </div>
+
+
+            <div class="flex-1 overflow-hidden flex w-fit mx-auto">
+                <img
+                    :src="modalImage.value"
+                    class="object-contain h-full w-full"
+                >
+            </div>
+
+        </div>            
+
+        
+    </UModal>
 
 </template>
 
